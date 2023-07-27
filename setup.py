890891c7f7fe46d9de097c7d1751d88e728ef7d8
Shrikar Archak <shrikar84@gmail.com>
2023-07-26 23:34:55 -0700

    Fix readme path

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='chatsearch',
+    name='gpthistory',
     version='0.1',
+    description='A tool for searching through your chatgpt conversation history',
+    author='Shrikar Archak',
+    author_email='shrikar84@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Click',
+        'dotenv',
+        'openai',
+        'pandas',
+        'numpy'
     ],
     entry_points='''
         [console_scripts]
-        chatsearch=chatsearch.chatsearch:main
+        gpthistory=gpthistory.gpthistory:main
     ''',
 )
