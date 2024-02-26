b1a1a832dfb8c8eb10dab0ffdd908597984e6a33
Tony Okeke <tonykabilanokeke@gmail.com>
2024-02-26 00:19:50 -0500

    migrate to openai 1.0.0 API

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gpthistory',
-    version='0.3',
-    description='A tool for searching through your chatgpt conversation history',
-    author='Shrikar Archak',
-    author_email='shrikar84@gmail.com',
+    name="gpthistory",
+    version="0.3",
+    description="A tool for searching through your chatgpt conversation history",
+    author="Shrikar Archak",
+    author_email="shrikar84@gmail.com",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'Click',
-        'python-dotenv',
-        'openai',
-        'pandas',
-        'numpy'
-    ],
-    entry_points='''
+    install_requires=["typer", "python-dotenv", "openai", "pandas", "numpy"],
+    entry_points="""
         [console_scripts]
         gpthistory=gpthistory.gpthistory:main
-    ''',
+    """,
 )
